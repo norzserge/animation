cc5d636411e996b89f68be77932624252c786b19
norzserge <metelskiisa@mail.ru>
2022-07-29 08:52:33 +0300

    sticky-scroll with subheader

--- sticky-header/script.js
+++ sticky-header/script.js
@@ -1,10 +1,27 @@
 const header = document.querySelector('.header');
-const firstSection = document.querySelector('.part-1');
+const firstBlock = document.querySelector('.part-1');
+const scrolledHeaderStart = firstBlock.offsetHeight;
+
+const backScrolledHeader = header.querySelector('.header__backscrolled');
+
+let scrollStarted = 0;
 
 document.addEventListener('scroll', function() {
-  if (window.scrollY > firstSection.scrollHeight) {
+  let scrollY = window.scrollY;
+  
+  if (scrollY > scrolledHeaderStart) {
     header.classList.add('header--fixed');
   } else {
     header.classList.remove('header--fixed');
   }
+
+  let delta = scrollY - scrollStarted;
+
+  if ((delta < 0) && (scrollY >= scrolledHeaderStart)) {
+    backScrolledHeader.classList.add('header__backscrolled--show');
+  } else {
+    backScrolledHeader.classList.remove('header__backscrolled--show');
+  }
+
+  scrollStarted = scrollY;
 });
